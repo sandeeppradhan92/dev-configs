d1b4415d655b6f7ddbc41b5bcc53c16c42650c57
Sandeep Kumar Pradhan <sandeeppradhan92@gmail.com>
2021-09-08 16:08:12 +0530

    Create pre-setup.sh

---
+++ pre-setup.sh
@@ -0,0 +1,11 @@
+pip3 install argcomplete
+activate-global-python-argcomplete
+
+if [[ $OSTYPE == 'darwin'* ]]; then
+  brew install --cask snowflake-snowsql
+else
+  curl -O https://sfc-repo.snowflakecomputing.com/snowsql/bootstrap/1.2/linux_x86_64/snowsql-1.2.17-linux_x86_64.bash
+  chmod 777 snowsql-linux_x86_64.bash
+  SNOWSQL_DEST=~/bin SNOWSQL_LOGIN_SHELL=~/.profile bash snowsql-linux_x86_64.bash
+  rm -f snowsql-linux_x86_64.bash
+fi
