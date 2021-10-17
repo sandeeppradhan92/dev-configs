8145c65e4f2e132403be9bcd15b477fa4ecc1c76
Sandeep Kumar Pradhan <sandeeppradhan92@gmail.com>
2021-10-17 14:53:04 +0530

    Update pre-setup.sh

--- pre-setup.sh
+++ pre-setup.sh
@@ -27,6 +27,7 @@ else
 fi
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+rm -rf ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 pip3 install ptpython
