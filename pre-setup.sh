261cbe8d722e90829de6fbceea4adc5c4abc7736
Sandeep Kumar Pradhan <sandeeppradhan92@gmail.com>
2021-10-17 16:23:26 +0530

    Update pre-setup.sh

--- pre-setup.sh
+++ pre-setup.sh
@@ -35,6 +35,9 @@ fi
 curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash
 rm -rf ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+rm -rf ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 
 python3 -m pip install ptpython
 python3 -m pip install argcomplete
