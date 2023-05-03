27cc3143106ae77c5555be66adf8625a83b38f43
Sandeep Kumar Pradhan <sandeeppradhan92@gmail.com>
2023-05-03 16:46:46 +0530

    Update pre-setup.sh

--- pre-setup.sh
+++ pre-setup.sh
@@ -3,31 +3,43 @@
 SNOW_VER=1.2.17
 
 if [[ $OSTYPE == 'darwin'* ]]; then
+  # Install helix editor
+  brew install helix
   brew install kubectl 
   brew install --cask snowflake-snowsql
   # Print system information in cli . Use case [ neofetch ]
   brew install neofetch
-  # for printing banner style output in command line alternative [ banner <text> ] 
+  # For printing banner style output in command line alternative [ banner <text> ] 
   brew install figlet
-  # install ubuntu like tree command
+  # Install ubuntu like tree command
   brew install tree
-  # install watch command in Mac . Use case [ watch {command} ]
+  # Install watch command in Mac . Use case [ watch {command} ]
   brew install watch
+  # Install Podman
+  brew install podman
+  # Install minikube
+  brew install minikube
+  brew unlink minikube
+  brew link minikube
 else
   ####### For now only tested with Ubuntu >= 16.04 and ubutnu in wsl2 #####
-  # install python pip
+  # Install helix editor
+  sudo add-apt-repository ppa:maveonair/helix-editor
+  sudo apt update
+  sudo apt install helix
+  # Install python pip
   sudo apt-get update -y
   sudo apt install python3-pip -y
   # Print system information in cli . Use case [ neofetch ]
   sudo apt-get install neofetch
-  # for printing banner style output in command line alternative [ banner <text> ] 
+  # For printing banner style output in command line alternative [ banner <text> ] 
   sudo apt-get install figlet
-  ## install zsh
+  # Install zsh
   rm -rf $HOME/.oh-my-zsh
   sudo apt install zsh -y
   ## Add zsh to /etc/shells:
   command -v zsh | sudo tee -a /etc/shells
-  ## use chsh to set zsh as default shell:
+  ## Use chsh to set zsh as default shell:
   sudo chsh -s "$(command -v zsh)" "${USER}"
   ## Install kubectl
   curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
@@ -37,16 +49,19 @@ else
   chmod +x kubectl
   mkdir -p ~/.local/bin/kubectl
   mv ./kubectl ~/.local/bin/kubectl
-  ## install snowsql
+  ## Install snowsql
   curl -O https://sfc-repo.snowflakecomputing.com/snowsql/bootstrap/1.2/linux_x86_64/snowsql-${SNOW_VER}-linux_x86_64.bash
   mv snowsql-${SNOW_VER}-linux_x86_64.bash snowsql-linux_x86_64.bash
   chmod 777 snowsql-linux_x86_64.bash
   SNOWSQL_DEST=~/bin SNOWSQL_LOGIN_SHELL=~/.profile bash snowsql-linux_x86_64.bash
   rm -f snowsql-linux_x86_64.bash
-  # configure python cli argument completion
+  # Configure python cli argument completion
   python3 -m pip install argcomplete
   activate-global-python-argcomplete
-  
+  # Install Podman
+  sudo apt-get -y install containernetworking-plugins
+  sudo apt-get -y install podman
+  # Install minikube
 fi
 
 # Install ZSH specific tools, themes and plugins
